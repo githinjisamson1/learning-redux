97d1e9ced6dee04d060a4921e052bc872bb5a1d3
Samson Githinji <githinjisamson148@gmail.com>
2023-11-18 05:17:09 +0300

    Setup iceCreamSlice.js

---
+++ rtk-demo/features/iceCream/iceCreamSlice.js
@@ -0,0 +1,27 @@
+// createSlice
+const createSlice = require("@reduxjs/toolkit").createSlice;
+
+// initialState
+const initialState = {
+  numberOfIceCreams: 20,
+};
+
+// iceCreamSlice
+const iceCreamSlice = createSlice({
+  name: "iceCream",
+  initialState,
+  reducers: {
+    ordered: (state, action) => {
+      state.numberOfIceCreams -= action.payload;
+    },
+    restocked: (state, action) => {
+      state.numberOfIceCreams += action.payload;
+    },
+  },
+});
+
+// default export
+module.exports = iceCreamSlice.reducer;
+
+// named export
+module.exports.iceCreamActions = cakeSlice.actions;
