a742c754872992b33dec5e25076a568abb84f058
Samson Githinji <githinjisamson148@gmail.com>
2023-11-19 08:44:19 +0300

    Understand useSelector/useDispatch usage in IceCreamView.jsx

--- react-rtk-demo/src/features/iceCream/IceCreamView.jsx
+++ react-rtk-demo/src/features/iceCream/IceCreamView.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { iceCreamActions } from "./iceCreamSlice";
 
 const IceCreamView = () => {
-  return (
-    <div>IceCreamView</div>
-  )
-}
+  const numberOfIceCreams = useSelector((state) => {
+    return state.iceCream.numberOfIceCreams;
+  });
 
-export default IceCreamView
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h1>Number of ice creams - {numberOfIceCreams}</h1>
+      <button
+        onClick={() => {
+          dispatch(iceCreamActions.ordered(1));
+        }}
+      >
+        Order ice cream
+      </button>
+      <button
+        onClick={() => {
+          dispatch(iceCreamActions.restocked(2));
+        }}
+      >
+        Restock ice cream
+      </button>
+    </div>
+  );
+};
+
+export default IceCreamView;
